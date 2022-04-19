f06256483b7d590ac9cae997143a73a7a83697f1
escowin <edwin@escowinart.com>
2022-04-19 05:59:50 -0500

    12.2.4 | db queries

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { param } = require('express/lib/request');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -19,9 +20,36 @@ const db = mysql.createConnection(
     console.log('connected to election db')
 );
 
-db.query('SELECT * FROM candidates', (err, rows) => {
-    console.log(rows);
-});
+// db.query('SELECT * FROM candidates', (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET single candidate
+// db.query('SELECT * FROM candidates WHERE id = 1', (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE candidate
+// db.query('DELETE FROM candidates WHERE id = ?', 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// CREATE candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbanks', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
