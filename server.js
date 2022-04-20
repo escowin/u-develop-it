3ea5264c40deeaf8f8aa04bb82babd23dad56870
escowin <edwin@escowinart.com>
2022-04-19 20:22:22 -0500

    12.2.8 | add crud routes

--- server.js
+++ server.js
@@ -87,19 +87,19 @@ app.post('/api/candidate', ({ body }, res) => {
     return;
   }
 
-  const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
-  const params = [body.first_name, body.last_name, body.industry_connected];
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
-    });
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'success',
+    data: body
   });
+}););
 });
 
 // db.query(sql, params, (err, result) => {
